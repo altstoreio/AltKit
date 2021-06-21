23bee561948f7ede8c499a0bd2ba3a2ae1ba1112
Riley Testut <riley@rileytestut.com>
2021-06-21 11:09:08 -0700

    Adds tvOS support

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "AltKit",
     platforms: [
         .iOS(.v12),
+        .tvOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
